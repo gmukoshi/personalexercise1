1621c969a0d71959b9d370877aeffb27c786a103
George <mukoshig@gmail.com>
2025-06-30 19:55:25 +0300

    README.md

---
+++ Arrays-Object revision.js
@@ -0,0 +1,12 @@
+let letters= ['a','b','c','d']
+console.log(letters [0]) ;
+console.log(letters [1]);
+
+let book ={
+    title : 'kifo kisimani',
+    author: 'Ken walibora',
+    Publisher : 'O\'Riley'
+}
+book.title ='I know JS'
+
+console.log(book.title)
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # personalexercise1
+This is my workspace for phase 1 where i get to try our JavaScript Fundamentals :
+- Functions
+- Arrays
+- Objects
+- DOM
+## Arrays and Object Revision
+
